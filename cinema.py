845ebe25291e0dba7eced5d94229b6a26e1fc9ef
Otu Ekanem <io2@users.noreply.github.com>
2021-11-29 15:22:35 +0100

    added small standalone program test

---
+++ cinema.py
@@ -0,0 +1,29 @@
+
+"""
+The page shown at the link below contains movies with some metadata of interest to our friend Quentin, a budding movie director.
+
+https://www.listchallenges.com/100-must-see-movies-for-more-advanced-cinephiles
+
+title
+year
+ranking
+no_of_votes
+
+
+Task
+Using your preferred language and/or tools - write a program that parses this page and extracts the following data into two possible file formats
+
+1 - A CSV file
+2 - A HTML file with some style formatting applied - You can use a CSS framework like https://tailwindcss.com/docs to complete this task.
+
+
+Include instructions on how to run your program including installing any dependencies.
+
+Usage:
+
+python cinema.py --format [ CSV | HTML ]
+
+"""
+
+def transform():
+  pass
